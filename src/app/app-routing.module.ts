9feaf1736b16c74ba10802ffe1ec7ead334436a9
Dustin Herboldshimer <dustnpdx@gmail.com>
2019-06-14 11:11:41 -0700

    create FrontPageComponent, create route and set as default route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FrontPageComponent } from './front-page/front-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/Medium', pathMatch: 'full'},
+  { path: 'Medium', component: FrontPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
