bbc167120fbb0968120434fc8c2d5cfd0b9027fa
Dustin Herboldshimer <dustnpdx@gmail.com>
2019-06-14 09:59:14 -0700

    create Article Object

---
+++ src/app/articles.model.ts
@@ -0,0 +1,13 @@
+export class Article {
+  id: number;
+  title: string;
+  tagLine: string;
+  author: string;
+  imgUrl: string;
+  pubDate: string;
+  readLength: number;
+  body: string;
+  mainCategory: string;
+  tags: string[];
+  claps: number;
+}
