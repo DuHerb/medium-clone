af8cd70bd43b21f366d4c3f4fa8fb62133f01850
Dustin Herboldshimer <dustnpdx@gmail.com>
2019-06-21 14:27:13 -0700

    modify model to set default values on construction

--- src/app/article.model.ts
+++ src/app/article.model.ts
@@ -1,4 +1,11 @@
 export class Article {
+  id: number;
+  imgUrl: string;
+  pubDate: string;
+  readTime: string;
+  tags: string[];
+  claps: number;
+
   constructor(
     public title: string,
     public tagline: string,
@@ -6,14 +13,18 @@ export class Article {
     public category: string,
     public body: string,
     public readtime: string,
-    public isFeatured: boolean
-  ) {
+    public isFeatured: boolean,
+    ) {
+      this.id = 0;
+      this.imgUrl = './../assets/img/seedImage.jpeg';
+      this.pubDate = 'Default';
+      this.claps = 0;
+      this.tags = [];
+    }
 
+    getData(): object {
+      const result = {};
+      Object.keys(this).map(key => result[key] = this[key]);
+      return result;
   }
-  id: number;
-  imgUrl: string;
-  pubDate: string;
-  readTime: string;
-  tags: string[];
-  claps: number;
 }
