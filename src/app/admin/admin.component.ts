b13f7495379cf5bc5f9a1fce8247cdc8a780c83f
Dustin Herboldshimer <dustnpdx@gmail.com>
2019-06-21 14:29:46 -0700

    update submitNewArticle()

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -10,11 +10,18 @@ import { ParsedEventType } from '@angular/compiler';
 })
 export class AdminComponent implements OnInit {
 
-  constructor(private artService: ArticleService) { }
+  constructor(private artService: ArticleService) {}
 
-  submitNewArticle(title: string, author: string, tagline: string, isFeatured: boolean, category: string, readtime: string, body: string) {
+  submitNewArticle(title: string,
+                   author: string,
+                   tagline: string,
+                   isFeatured: boolean,
+                   category: string,
+                   readtime: string,
+                   body: string) {
     const newArticle = new Article(title, tagline, author, category, body, readtime, isFeatured);
     console.log(newArticle);
+    this.artService.addFSarticle(newArticle);
   }
 
   ngOnInit() {
