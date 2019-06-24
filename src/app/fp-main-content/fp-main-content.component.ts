9c6236a14cbe73ca21f5f15625de9e96c4447285
Dustin Herboldshimer <dustnpdx@gmail.com>
2019-06-23 23:20:13 -0700

    catch all commit after firebase deployment

--- src/app/fp-main-content/fp-main-content.component.ts
+++ src/app/fp-main-content/fp-main-content.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Article } from './../article.model';
 
 @Component({
   selector: 'app-fp-main-content',
@@ -7,7 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FpMainContentComponent implements OnInit {
 
+  article: Article;
+  featuredArticles: Article[];
+
   constructor() { }
+
+  getUrl(article: Article) {
+    return article.imgUrl;
+  }
 
   ngOnInit() {
   }
